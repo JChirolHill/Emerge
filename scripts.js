42e530ab64c112d16e9c3272ff8827d8e13eb1bc
J Hill <chirolhi@usc.edu>
2020-12-23 17:40:13 -0800

    Vanilla state version

--- scripts.js
+++ scripts.js
@@ -1,52 +1,19 @@
-let raws = [];
 let lines = [];
+let state = 0;
 
 // ------------------------------------------------------------
 // Step 0: Enter raw original text and correction text
 // ------------------------------------------------------------
 
-// Onclick for Enter Another button
-document.querySelector('#step-0 .enter-raw').onclick = () => {
-    // Add input to raw array
-    let textarea = document.querySelector('#step-0 textarea');
-    if (textarea.value.trim() !== '') {
-        raws.push(textarea.value);
-        textarea.value = "";
-
-        // Change radio selection to correction
-        document.querySelector('#step-0 input[value=correction]').checked = true;
-    }
-};
-
 // Onclick for Step 0 Continue button
 document.querySelector('#step-0 .continue').onclick = () => {
-    // Add input to raw array
     let textarea = document.querySelector('#step-0 textarea');
-    if (textarea.value.trim() !== '') {
-        raws.push(textarea.value);
-        textarea.value = "";
-
-        // Show spinny
-
-        // Separate out lines for all raw inputs
-        raws.forEach((raw) => {
-            let rawLines = [];
-            let tempLines = raw.split(/\n/);
-            tempLines.forEach((line) => {
-                rawLines = rawLines.concat(line.split(/\.|。|!|\?/));
-            });
-            rawLines.forEach((line, index) => {
-                if (line === '') {
-                    rawLines.splice(index, 1);
-                }
-            });
-
-            lines.push(rawLines);
-        });
-
-        // Populate step 1 and show
-        populateCheckLines(1, 0);
-        // TODO: show step 1
+    if (textarea.value.trim() === '') {
+        // TODO show error message
+        // Please input something before pressing continue
+    }
+    else {
+        loadNextState();
     }
 };
 
@@ -54,20 +21,19 @@ document.querySelector('#step-0 .continue').onclick = () => {
 // Step 1: Validate lines in original
 // ------------------------------------------------------------
 
+document.querySelector('#step-1 .reset').onclick = () => {
+    populateTextarea(1, 0);
+};
+
 document.querySelector('#step-1 .continue').onclick = () => {
-    updateLines(1, 0);
-
-    // Populate step 2 original side
-    lines[0].forEach((line) => {
-        let tempDiv = document.createElement('div');
-        tempDiv.innerText = line;
-        document.querySelector('#step-2 .list-orig-lines').appendChild(tempDiv);
-    });
-
-    // Populate step 2 correction side
-    populateCheckLines(2, 1);
-
-    // TODO Show step 2
+    let textarea = document.querySelector('#step-1 textarea');
+    if (textarea.value.trim() === '') {
+        // TODO show error message
+        // Please input something before pressing continue
+    }
+    else {
+        loadNextState();
+    }
 };
 
 // ------------------------------------------------------------
@@ -99,30 +65,117 @@ function updateLines(domStep, correctionIndex) {
 }
 
 // Populates container for checks and text inputs to validate lines
-function populateCheckLines(domStep, correctionIndex) {
-    let linesContainer = document.querySelector(`#step-${domStep} .list-check-lines`);
+// function populateCheckLines(domStep, correctionIndex) {
+//     let linesContainer = document.querySelector(`#step-${domStep} .list-check-lines`);
+//
+//     // Add each line to the linesContainer
+//     lines[correctionIndex].forEach((line, index) => {
+//         let tempDiv = document.createElement('div');
+//         let tempInputCheck = document.createElement('input');
+//         let tempInputText = document.createElement('input');
+//         let forAttr = document.createAttribute('for');
+//
+//         tempInputCheck.type = 'checkbox';
+//         tempInputCheck.checked = true;
+//         tempInputCheck.value = line;
+//         tempInputText.type = 'text';
+//         tempInputText.value = line;
+//
+//         tempDiv.appendChild(tempInputCheck);
+//         tempDiv.appendChild(tempInputText);
+//         linesContainer.appendChild(tempDiv);
+//     });
+//
+//     // Show number of lines only if not validating original
+//     if (domStep >= 2) {
+//         let matchStatus = document.querySelector(`#step-${domStep} .correction-container .match-status`);
+//         matchStatus.innerText = `${lines[correctionIndex].length} Lines`;
+//     }
+// }
 
-    // Add each line to the linesContainer
-    lines[correctionIndex].forEach((line, index) => {
-        let tempDiv = document.createElement('div');
-        let tempInputCheck = document.createElement('input');
-        let tempInputText = document.createElement('input');
-        let forAttr = document.createAttribute('for');
+function splitLinesFromRaw(raw) {
+    // Split raw by new-line character
+    let rawSplitNewLine = raw.split(/\n/);
 
-        tempInputCheck.type = 'checkbox';
-        tempInputCheck.checked = true;
-        tempInputCheck.value = line;
-        tempInputText.type = 'text';
-        tempInputText.value = line;
-
-        tempDiv.appendChild(tempInputCheck);
-        tempDiv.appendChild(tempInputText);
-        linesContainer.appendChild(tempDiv);
+    // Split by end-of-sentence punctuation
+    let rawSplit = [];
+    rawSplitNewLine.forEach((line) => {
+        rawSplit = rawSplit.concat(line.split(/\.|。|!|\?/));
     });
 
-    // Show number of lines only if not validating original
-    if (domStep >= 2) {
-        let matchStatus = document.querySelector(`#step-${domStep} .correction-container .match-status`);
-        matchStatus.innerText = `${lines[correctionIndex].length} Lines`;
+    // Remove any blank lines after splitting
+    rawSplit.forEach((line, index) => {
+        if (line === '') {
+            rawSplit.splice(index, 1);
+        }
+    });
+
+    return rawSplit;
+}
+
+function populateTextarea(domStep, indexCorrection) {
+    let textarea = document.querySelector(`#step-${domStep} textarea`);
+    textarea.value = '';
+    lines[indexCorrection].forEach((line) => {
+        textarea.value += `${line}\n`;
+    });
+}
+
+function loadNextState() {
+    switch (state) {
+        case 0:
+            if (lines.length > 0) { // Inputted a correction
+                // if num lines matches original
+                // Go to step 0
+                // state = 0;
+                // else
+                // Go to step 2
+                // state = 2;
+            }
+            else if (lines.length === 0) { // Inputted original
+                // Split lines
+                let textarea = document.querySelector(`#step-0 textarea`);
+                let rawSplit = splitLinesFromRaw(textarea.value);
+                textarea.value = "";
+                lines.push(rawSplit);
+
+                // Populate step 1
+                populateTextarea(1, 0);
+
+                state = 1;
+                // TODO: Hide step 0, show step 1
+            }
+            break;
+        case 1:
+            // Split lines
+            let textarea = document.querySelector(`#step-1 textarea`);
+            let rawSplit = splitLinesFromRaw(textarea.value);
+            textarea.value = "";
+            lines[lines.length - 1] = rawSplit;
+
+            // Setup step 0 for accepting corrections
+            document.querySelector('#step-0 .instructions').innerText = 'Copy and paste each correction';
+
+            // Populate step 2 original side
+            // lines[0].forEach((line) => {
+            //     let tempDiv = document.createElement('div');
+            //     tempDiv.innerText = line;
+            //     document.querySelector('#step-2 .list-orig-lines').appendChild(tempDiv);
+            // });
+            //
+            // Populate step 2 correction side
+            // populateCheckLines(2, 1);
+            //
+            // // TODO Show step 2
+
+            state = 0;
+            // TODO: hide step 1, show step 0
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            console.log('Error, nonexistent state');
     }
 }
